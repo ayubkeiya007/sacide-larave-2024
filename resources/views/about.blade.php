3271edea8d2eb5b2182393ad6dbd856e88b6cd92
ayub keiya <ayubkeiya007@gmail.com>
2024-10-17 20:13:19 +0900

    instal laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman about</title>
+</head>
+<body>
+<h1>About</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis fugiat inventore optio a? Tempora fugiat a eos architecto, ad facilis quas repudiandae eligendi dolor? Quasi blanditiis impedit magnam saepe hic! Voluptates blanditiis eum soluta natus qui eius, in labore qgiuo eveniet cupiditate delectus illo quas animi fugiat corporis commodi aut?</p>   
+</body>
+</html>
